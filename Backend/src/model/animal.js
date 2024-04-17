257e2c661b0e07c6719bea308114baf3412b32c3
AndrewChian2 <123596978+AndrewChian2@users.noreply.github.com>
2024-04-16 19:59:40 -0500

    threat level config

--- Backend/src/model/animal.js
+++ Backend/src/model/animal.js
@@ -2,20 +2,20 @@ import { prisma } from "../../prisma.config.js";
 
 
 export const updateAnimalThreatLevel = async (animalId, newThreatLevel) => {
-    const updatedAnimal = await prisma.animal.update({
-      where: { id: animalId },
-      data: { threatLevel: newThreatLevel },
-    });
-    return updatedAnimal;
-  };
+  const updatedAnimal = await prisma.animal.update({
+    where: { id: animalId },
+    data: { threatLevel: newThreatLevel },
+  });
+  return updatedAnimal;
+};
 
 export const getAnimalId = async (userId, animalName) => {
   const animal = await prisma.animal.findMany({
-    where : {
-      name:animalName,
-      userId:userId
+    where: {
+      name: animalName,
+      userId: userId
     }
   });
-  
+
   return animal[0]?.id;
 };
